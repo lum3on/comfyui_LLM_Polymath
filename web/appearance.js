7cd54641cb1556dee1b3ede9348c8d7cd5b9edda
benjamin <benjaminbertram@gmail.com>
2025-02-04 21:15:01 +0100

    first commit

--- web/appearance.js
+++ web/appearance.js
@@ -1,8 +1,13 @@
 import { app } from "../../scripts/app.js";
 
 app.registerExtension({
+<<<<<<< HEAD
     name: "LLM-Polymath.appearance", // Extension name - was ist der richtige Name hier?
     async nodeCreated(node) {
+=======
+    name: "polymath.appearance", // Extension name - was ist der richtige Name hier?
+    nodeCreated(node) {
+>>>>>>> da240e8 (first commit)
         if (node.comfyClass === "polymath") {
             // Apply styling
             node.color = "#16727c";
