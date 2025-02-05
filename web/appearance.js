8ca1511dd60c4ce54f6a88286d79ab78cc4df655
benjamin <benjaminbertram@gmail.com>
2025-02-05 15:42:13 +0100

    update pyproject

--- web/appearance.js
+++ web/appearance.js
@@ -1,14 +1,9 @@
 import { app } from "../../scripts/app.js";
 
 app.registerExtension({
-<<<<<<< HEAD
-    name: "LLM-Polymath.appearance", // Extension name - was ist der richtige Name hier?
-    async nodeCreated(node) {
-=======
     name: "polymath.appearance", // Extension name - was ist der richtige Name hier?
-    nodeCreated(node) {
->>>>>>> da240e8 (first commit)
-        if (node.comfyClass === "polymath") {
+    async nodeCreated(node) {
+        if (node.comfyClass === "polymath") { // was ist der richtige Name hier?
             // Apply styling
             node.color = "#16727c";
             node.bgcolor = "#4F0074";
